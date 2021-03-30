4182472885c6c4090b1ef93280a1b44f2f82c086
Ivan Shitikov <ifinesse2406@gmail.com>
2021-03-30 19:52:18 +0300

    "45th lesson finished"

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Alert} from 'react-native';
+import { StyleSheet, View, Alert, Text} from 'react-native';
 import * as Font from 'expo-font'
 import AppLoading from 'expo-app-loading'
-import { Navbar } from './src/components/Navbar.jsx'
-import { MainScreen } from './src/screens/MainScreen.jsx'
-import { TodoScreen } from './src/screens/TodoScreen.jsx';
-import {THEME} from './src/theme'
+import { MainLayout } from './src/MainLayout.js';
+import {TodoState} from './src/context/todo/TodoState'
 
 
 async function loadApplication () {
@@ -20,19 +18,6 @@ export default function App() {
 
   const [isReady, setIsReady] = useState(false)
 
-  const [todoId, setTodoId] = useState(null)
-
-  const [todos, setTodo] = useState([
-    {
-      id: "1",
-      title: "abc"
-    },
-    {
-      id: "2",
-      title: "def"
-    },
-  ])
-
     if(!isReady) {
       return (<AppLoading 
         startAsync={loadApplication}
@@ -40,98 +25,14 @@ export default function App() {
         onFinish={() => setIsReady(true)}
       />)
     }
-
-    // let [fontsLoaded] = Font.useFonts({
-      // 'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
-      // 'Inter-SemiBoldItalic': 'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12',
-    //   'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
-    // 'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf')
-    // });
-  
-
-  const addTodo = (title) => {
-
-    const newTodo = {
-      id: Date.now().toString(),
-      title,
-    }
-
-    setTodo((prev) => [...prev, newTodo])
-
-  }
-
-  const removeTodo = id => {
-
-    const todo = todos.find(t => t.id === id);
-
-    Alert.alert(
-      "Deleting item",
-      `Are you sure you want to delete "${todo.title}"?`,
-      [
-        {
-          text: "Cancel",
-          style: "cancel"
-        },
-        {
-          text: "OK", onPress: () => {
-            setTodoId(null);
-            setTodo(prev => prev.filter(todo => todo.id !== id))
-          }
-        },
-      ],
-      {
-        cancelable: true,
-        onDismiss: () => {
-          Alert.alert("this alert was dismissed by clicking outside the alert dialog")
-        }
-      }
-    );
-
-  }
-
-  const updateTodo = (id, title) => {
-    setTodo(prev => prev.map(todo => {
-      if (todo.id === id) {
-        todo.title = title
-      }
-      return todo;
-    }))
-  }
-
-  let content = (
-    <MainScreen todos={todos} addTodo={addTodo} removeTodo={removeTodo} openTodo={setTodoId} />
-  )
-
-  if (todoId) {
-    const todo = todos.find(todo => todo.id === todoId)
-    content = (
-      <TodoScreen
-        goBack={() => setTodoId(null)}
-        todo={todo} 
-        removeTodo={removeTodo}
-        onSave={updateTodo}
-      />
-    )
-  }
   
   return (
-    <View>
-
-      <Navbar title={"todo app"} />
-      <View style={styles.container}>
-          {content}
-      </View>
-      
-
-
-    </View>
+   <TodoState>
+     <MainLayout/>
+    {/* <Text>111</Text> */}
+   </TodoState>
+  // <View></View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    paddingHorizontal: THEME.PADDING_HORIZONTAL,
-    paddingVertical: THEME.PADDING_VERTICAL,
-  },
-});
+
